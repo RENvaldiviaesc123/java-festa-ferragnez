a102227146915727eb6e38e5dc8bb41dd7028a7a
renat <guitarra123primera@gmail.com>
2023-07-13 15:21:27 +0200

    Pages Setup

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,2 +1,18 @@
-package com.ferragnez.party;public class CheckGuest {
+package com.ferragnez.party;
+/*
+Consegna: creare un progetto java-festa-ferragnez con il package com.ferragnez.party,
+a cui aggiungere la classe CheckGuest
+Nel programma bisogna:
+creare e inizializzare l’array contenente i nomi degli invitati
+chiedere all’utente come si chiama
+verificare che il nome sia presente nella lista (:esclamazione:ATTENZIONE: in Java per
+confrontare due stringhe non posso usare ==, ma devo usare il metodo equals(): esempio qui)
+lasciarlo entrare o rispedirlo cortesemente da dove è venuto .
+ */
+//Importiamo il nostro array
+
+public class CheckGuest {
+    public static void main(String[] args) {
+
+    }
 }
