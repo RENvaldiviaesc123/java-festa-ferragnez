066827b1230629b161394ce17108bfb1eb6dae24
renat <guitarra123primera@gmail.com>
2023-07-17 10:00:49 +0200

    Correzione con ciclo for e anche del bonus con ciclo while.

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -27,13 +27,14 @@ public class CheckGuest {
         System.out.println("Inserisca il suo nome: ");
         String userName = scanGuest.nextLine();
 
-
+        /*
         //Usiamo un ciclo for per verificare se il nome è dentro il nostro array
         boolean found = false;
         for (int i = 0; i < arrayGuest.length; i++) {
             //AD OGNI ITERZIONE DEL CICLO CONFRONTO IL USERNAME CON L'ARRAY
                 if(arrayGuest[i].equalsIgnoreCase(userName)){ //questo equalsignorecase tiene conto delle parole senza l'uso delle maiuscole
                     found=true;
+                    break; //per fermare  il processo quando abbiamo trovato ciò che cercavamo
                 }//Non mettiamo un else perche abbiamo inizializzato il nostro boolean con false ovvero col fatto che è falso
         }
         //Testiamo la varibile booleana
@@ -41,34 +42,46 @@ public class CheckGuest {
             System.out.println("Welcome!");
         } else {
             System.out.println("Sorry, you're not in the list");
-        }
-
-
-
+        }*/
         /*
-        //METODO 2 con variabile booleana
-
-        String[] arrayGuest = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti",
-                "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
-
-        //dichiariamo variabile booleana
-        boolean stop = false;
-        System.out.println("Inserisca il suo nome: ");
-        String userName = scanGuest.nextLine();
-        int i=0;
-        //Usiamo il nostro ciclo while
-        while (i<arrayGuest.length) {
-            userName.equals(arrayGuest);
-            stop=true;
+        //Altro modo per risolvere più elegante
+        boolean found = false;
+        for (int i = 0; i < arrayGuest.length && !found; i++) { //cerco finchè trovo
+            //AD OGNI ITERZIONE DEL CICLO CONFRONTO IL USERNAME CON L'ARRAY
+            if(arrayGuest[i].equalsIgnoreCase(userName)){ //questo equalsignorecase tiene conto delle parole senza l'uso delle maiuscole
+                found=true;
+                //break; così non è più necessario usare il break
+            }//Non mettiamo un else perche abbiamo inizializzato il nostro boolean con false ovvero col fatto che è falso
         }
-        /*
-        //Ciclo FOR
-        for (int i = 0; i < arrayGuest.length; i++) {
-            userName.equals(arrayGuest[i]);
-            stop = true;
+        //Testiamo la varibile booleana
+        if(found) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Sorry, you're not in the list");
         }*/
 
 
+
+
+        //METODO 2 con ciclo while
+
+        //dichiariamo variabile booleana
+        boolean found= false;
+
+
+        int counter = 0; //questo fa un po quello che fa i nel ciclo for
+        //Usiamo il nostro ciclo while
+        while (!found && counter < arrayGuest.length) {//iteriamo finchè found è true e per tutta la lunghezza di arrayguest
+            if (arrayGuest[counter].equalsIgnoreCase(userName)) {
+            //ti ho trovato
+            found = true;}
+            counter++; //ad ogni iterazione aumento il contatore
+        }
+        if(found) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Sorry, you're not in the list");
+        }
 
         //Chiudiamo il nostro scanner
         scanGuest.close();
